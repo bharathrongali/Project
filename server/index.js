d88abd1c0d5ae5e302379ef855360c3df74129a8
bharathrongali <rongalibharath33@gmail.com>
2022-05-06 17:15:21 +0500

    Revert "merge conflict"
    This reverts commit d4bfe3d032f9ed466787d2a7d5a556cbc78a1d3c.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,62 @@
-const http = require('http');
+require('dotenv').config();
+const express = require('express');
+const { join } = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const userModel = require('./models/user');
+const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+const { requireAuth } = require('./models/auth');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+	.use('/', express.static(join(__dirname, '../client/dist')))
+
+	.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header(
+			'Access-Control-Allow-Headers',
+			'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		);
+		next();
+	})
+
+	.use(express.json())
+
+	.use((req, res, next) => {
+		const auth = req.headers.authorization;
+		if (auth) {
+			const token = auth.split(' ')[1];
+			userModel
+				.fromToken(token)
+				.then(user => {
+					req.user = user;
+					next();
+				})
+				.catch(next);
+		} else {
+			next();
+		}
+	})
+
+	.get('/api/', (req, res) => {
+		res.send('You are at the root of the API.');
+	})
+	.use('/api/users', usersController)
+	.use('/api/posts', /*requireAuth,*/ postsController)
+	.use((err, req, res, next) => {
+		console.error(err);
+
+		res
+			.status(err.statusCode || 500)
+			.send({ errors: [err.message ?? 'Internal server error'] });
+	})
+	.get('*', (req, res) =>
+		res.sendFile(join(__dirname, '../client/dist/index.html'))
+	);
+
+app.listen(port, () => {
+	console.log(`Listening at http://localhost:${port}`);
 });
---
+++ server/models/user.js
@@ -0,0 +1,126 @@
+/* B"H
+ */
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { db, isConnected, ObjectId } = require('./mongo');
+
+const collection = db.db('DB_APP').collection('users');
+
+const SALT_ROUNDS = +process.env.SALT_ROUNDS;
+
+let hieghstId = 3;
+
+const list = [
+	{
+		username: 'Bob',
+		password: '123',
+		avatar: "https://randomuser.me/api/portraits/men/7.jpg"
+	},
+	{
+		username: 'Alice',
+		password: '456',
+		avatar: "https://randomuser.me/api/portraits/men/8.jpg"
+	},
+];
+
+async function get(id) {
+	const user = await collection.findOne({ _id: new ObjectId(id) });
+	if (!user) {
+		throw { statusCode: 404, message: 'User not found' };
+	}
+	return { ...user, password: undefined };
+}
+
+async function getByHandle(handle) {
+	const user = await collection.findOne({ handle });
+	if (!user) {
+		throw { statusCode: 404, message: 'User not found' };
+	}
+	return { ...user, password: undefined };
+}
+
+async function remove(id) {
+	const user = await collection.findOneAndDelete({ _id: new ObjectId(id) });
+
+	return { ...user.value, password: undefined };
+}
+
+async function update(id, newUser) {
+	if (newUser.password) {
+		newUser.password = await bcrypt.hash(newUser.password, SALT_ROUNDS);
+	}
+
+	newUser = await collection.findOneAndUpdate(
+		{ _id: new ObjectId(id) },
+		{ $set: newUser },
+		{ returnDocument: 'after' }
+	);
+
+	return { ...newUser, password: undefined };
+}
+
+async function login(username, password) {
+	const user = await collection.findOne({ username });
+	if (!user) {
+		throw { statusCode: 404, message: 'User not found' };
+	}
+	if (!(await bcrypt.compare(password, user.password))) {
+		throw { statusCode: 401, message: 'Invalid password' };
+	}
+
+	const data = { ...user, password: undefined };
+	const token = jwt.sign(data, process.env.JWT_SECRET);
+
+	return { ...data, token };
+}
+
+function fromToken(token) {
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(decoded);
+			}
+		});
+	});
+}
+
+const seed = () => {
+	const l = list.map(e => ({
+		...e,
+		password: bcrypt.hashSync(e.password, SALT_ROUNDS),
+	}));
+	return collection.insertMany(l);
+};
+
+module.exports = {
+	collection,
+	seed,
+	getByHandle,
+	async create(user) {
+		user.id = ++hieghstId;
+
+		if (!user.handle) {
+			throw { statusCode: 400, message: 'Handle is required' };
+		}
+		user.password = await bcrypt.hash(user.password, +process.env.SALT_ROUNDS);
+		console.log(user);
+
+		const result = await collection.insertOne(user);
+		user = await get(result.insertedId);
+
+		return { ...user, password: undefined };
+	},
+	remove,
+	update,
+	login,
+	fromToken,
+	async getList() {
+		return (await collection.find().toArray()).map(x => ({
+			...x,
+			password: undefined,
+		}));
+	},
+};
+module.exports.get = get;
